072b95f5658ac6acd8ebf989bbf16582dde46e51
lightingggcontact-star <lightingggcontact@gmail.com>
2026-01-01 18:43:36 +0100

    🚑 Fix last apostrophe in admin page (line 320)

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -317,7 +317,7 @@ export default function AdminDashboard() {
                       className="w-full h-12"
                     >
                       <Plus className="w-5 h-5 mr-2" />
-                      Envoyer l'invitation
+                      Envoyer l&apos;invitation
                     </Button>
                   </div>
 
